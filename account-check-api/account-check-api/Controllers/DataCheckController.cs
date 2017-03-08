de5a1680dcf4e206f0c85984724e0293629bf048
tonipulkkinen <chipruli@gmail.com>
2017-03-08 14:44:27 +0200

    Created basic look for the web api

---
+++ account-check-api/account-check-api/Controllers/DataCheckController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace account_check_api.Controllers
+{
+    public class DataCheckController : ApiController
+    {
+        public string _accountNumber { get; set; }
+        public string _referenceNumber { get; set; }
+    }
+}
---
+++ account-check-api/account-check-api/Models/DataCheck.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace account_check_api.Models
+{
+    public class DataCheck
+    {
+    }
+}
